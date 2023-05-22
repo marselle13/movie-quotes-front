c945561d5f86d6545e94a2f0e1d56d822e03eda3
marselle13 <nika.gogua8@gmail.com>
2023-05-23 00:59:44 +0400

    style: button component

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="text-white rounded font-medium" :class="[buttonStyle, buttonAnimation]">
+    <slot></slot>
+  </button>
+</template>
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  mode: { type: String, required: false },
+  animation: { type: Boolean, required: false, default: false },
+})
+
+const buttonStyle = computed(() => {
+  return props.mode === 'flat'
+    ? 'bg-transparent border border-white hover:opacity-80'
+    : 'bg-[#E31221] hover:bg-[#CC0E10]'
+})
+
+const buttonAnimation = computed(() => {
+  return (
+    props.animation && 'transition ease-out delay-150 hover:ring hover:ring-[#F07C84] duration-300'
+  )
+})
+</script>
