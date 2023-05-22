d0e9d7a8ffb05515b04b36aaa5a276e75ed9b0e9
marselle13 <nika.gogua8@gmail.com>
2023-05-23 00:59:08 +0400

    style: dropdown component

---
+++ src/components/ui/BaseDropdown.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative">
+    <button class="w-full" type="button" @click="dropdownButton">
+      <slot name="dropdownButton"></slot>
+    </button>
+    <ul
+      v-if="dropdown"
+      class="z-10 bg-[#24222F] rounded-[10px] absolute py-2 text-sm text-white cursor-pointer"
+    >
+      <slot name="dropdown"></slot>
+    </ul>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+const dropdown = ref(false)
+
+function dropdownButton() {
+  dropdown.value = !dropdown.value
+}
+</script>
