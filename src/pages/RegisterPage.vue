643836e0acfb7e559cc9f5a82c3f4b4330f11d99
marselle13 <nika.gogua8@gmail.com>
2023-05-26 20:26:47 +0400

    chore: localization for register page

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,45 +1,46 @@
 <template>
   <auth-card>
-    <template #title> Create an account </template>
-    <template #info> Start your journey!</template>
+    <template #title> {{ t('create_account') }} </template>
+    <template #info> {{ t('start_journey') }}</template>
     <Form @submit="onSubmit" class="md:w-[450px] mt-5 space-y-4">
       <base-input
         id="name"
-        label="name"
-        :placeholder="isMobile ? 'Enter your name' : 'At least 3 & max.15 lower case characters'"
+        :label="t('name')"
+        :placeholder="t('enter_name')"
         rules="required|min:3|max:15|alpha_num"
       ></base-input>
       <base-input
         type="email"
         id="email"
-        label="email"
-        placeholder="Enter your email"
+        :label="t('email')"
+        :placeholder="t('enter_email')"
         rules="required|email"
       ></base-input>
       <base-input
         id="password"
-        label="password"
-        :placeholder="isMobile ? 'password' : 'At least 8 & max.15 lower case characters'"
+        :label="t('password')"
+        :placeholder="t('enter_password')"
         rules="required|min:8|max:15|alpha_num"
         ref="password"
       ></base-input>
       <base-input
         id="confirmation"
-        label="confirm password"
-        placeholder="Confirm password"
+        :label="t('confirm_password')"
+        :placeholder="t('confirm_password')"
         rules="required|confirmed:@password"
       ></base-input>
       <div class="pt-4 space-y-4 text-center">
-        <base-button class="w-full py-2">Get started</base-button>
+        <base-button class="w-full py-2">{{ t('get_started') }}</base-button>
         <base-button type="button" @click="signUpWithGoogle" mode="flat" class="w-full py-2">
           <div class="flex items-center justify-center gap-2">
-            <google-icon /> Sign up with Google
+            <google-icon /> {{ t('sign_up_google') }}
           </div>
         </base-button>
         <p class="text-[#6C757D]">
-          Already have an account?<router-link class="ml-1 text-[#0D6EFD] underline" to="/login"
-            >Log in</router-link
-          >
+          {{ t('have_account')
+          }}<router-link class="ml-1 text-[#0D6EFD] underline" to="#">{{
+            t('log_in')
+          }}</router-link>
         </p>
       </div>
     </Form>
@@ -53,7 +54,9 @@ import GoogleIcon from '@/components/icons/GoogleIcon.vue'
 import { Form } from 'vee-validate'
 import { useUserStore } from '@/stores/userStore'
 import { onMounted, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const userStore = useUserStore()
 const isMobile = ref(false)
 
