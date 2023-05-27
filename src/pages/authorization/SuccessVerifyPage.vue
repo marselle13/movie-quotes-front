938093830d161fe0225346dca32c3c89d4dbd170
marselle13 <nika.gogua8@gmail.com>
2023-05-28 01:24:13 +0400

    style: add succes verify page

---
+++ src/pages/authorization/SuccessVerifyPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <auth-card :wrapper="true">
+    <template #icon>
+      <SuccessCheck />
+    </template>
+    <template #title> Thank You!</template>
+    <div
+      class="flex flex-col justify-center items-center md:w-[385px] text-white text-center mt-8 space-y-10"
+    >
+      <p>Your account has been activated.</p>
+      <base-button :link="true" :to="{ name: 'login' }" class="w-48 md:w-full py-2"
+        >Go to my news feed</base-button
+      >
+    </div>
+  </auth-card>
+</template>
+<script setup>
+import SuccessCheck from '@/components/icons/SuccessCheck.vue'
+</script>
