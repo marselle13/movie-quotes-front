e914223a63478ba00b2761889b951f473ee48a43
marselle13 <nika.gogua8@gmail.com>
2023-06-23 01:28:06 +0400

    style: textarea compoenent

---
+++ src/components/ui/form/BaseTextarea.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex relative border border-[#6C757D]">
+    <textarea
+      :placeholder="placeholder"
+      style="min-height: 80px"
+      class="h-12 italic text-white w-full bg-transparent rounded outline-none pl-3 py-1 pr-16 lg:pr-24 placeholder-[#6C757D]"
+    ></textarea>
+    <h6 class="capitalize absolute right-4 top-2 text-white">{{ type }}</h6>
+  </div>
+</template>
+<script setup>
+defineProps({
+  placeholder: { type: String, required: false },
+  type: { type: String, required: false },
+})
+</script>
