450451ec92a0ce8a487b1439841fbf70ea865be3
marselle13 <nika.gogua8@gmail.com>
2023-06-23 01:28:27 +0400

    feat: upload file component

---
+++ src/components/ui/form/BaseUpload.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex justify-between lg:justify-start items-center border border-[#6C757D] rounded bg-transparent text-white gap-2 px-4 py-6"
+  >
+    <div class="flex items-center gap-2">
+      <CameraIcon />
+      <p class="sm:block lg:hidden hidden">Upload image</p>
+      <p class="hidden lg:block">Drag & drop your image here or</p>
+    </div>
+    <input id="image" type="file" class="sr-only" />
+    <label for="image" class="bg-[#9747ff7a] text-white p-2">Choose File</label>
+  </div>
+</template>
+<script setup>
+import CameraIcon from '@/components/icons/CameraIcon.vue'
+</script>
