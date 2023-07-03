fbeab461857077d719dc86dfb6b9639653ab164e
marselle13 <nika.gogua8@gmail.com>
2023-07-03 15:54:26 +0400

    chore: update not found redirect button

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,9 +3,15 @@
     <NotFoundIcon />
     <h1 class="text-2xl md:text-5xl text-white">Whoops!</h1>
     <p class="text-base md:text-2xl text-white">We can't see the page you are looking for</p>
-    <base-button link :to="{ name: 'landing' }" class="w-36 py-2">Return Home</base-button>
+    <base-button
+      link
+      :to="{ name: useUserStore().isAuth ? 'news-feed' : 'landing' }"
+      class="w-36 py-2"
+      >Return Home</base-button
+    >
   </section>
 </template>
 <script setup>
 import NotFoundIcon from '@/components/icons/NotFoundIcon.vue'
+import { useUserStore } from '@/stores/userStore'
 </script>
