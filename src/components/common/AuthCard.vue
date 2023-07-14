dce39129cb5871998c88d367bb46cbe5c8f51a9a
marselle13 <nika.gogua8@gmail.com>
2023-07-14 14:33:24 +0400

    style: update auth card style

--- src/components/common/AuthCard.vue
+++ src/components/common/AuthCard.vue
@@ -6,13 +6,16 @@
     ></div>
     <div class="grid justify-items-center h-auto relative">
       <div
-        class="md:bg-[#222030] fixed md:rounded-lg z-40 px-8 md:px-28 md:py-12 md:top-26 w-full md:h-auto overflow-hidden"
-        :class="{
-          'wrapper pt-20 pb-24 px-2 w-10/12 before:absolute before:w-full before:h-full before:top-0  before:left-0 before:opacity-30 before:backdrop-blur-xl before:rotate-180 rounded-lg before:-z-10':
-            wrapper,
-          'md:w-[600px]': notification,
-          'md:w-[650px]': !notification,
-        }"
+        class="md:bg-[#222030] fixed md:rounded-lg z-40 px-8 md:px-28 md:py-12 md:top-26 md:h-auto overflow-hidden"
+        :class="[
+          wrapper
+            ? 'wrapper pt-20 pb-24 px-2 w-10/12 before:absolute before:w-full before:h-full before:top-0  before:left-0 before:opacity-30 before:backdrop-blur-xl before:rotate-180 rounded-lg before:-z-10'
+            : 'w-full',
+          {
+            'md:w-[600px]': notification,
+            'md:w-[650px]': !notification,
+          },
+        ]"
       >
         <div class="text-center space-y-4 flex flex-col items-center">
           <slot name="icon"></slot>
